9c7bb583b4d150d0a1dafe1d5818e814f8522d83
root <root@vultr.guest>
2018-04-12 10:59:10 +0000

    can not mount dir to pgsql

--- src/http/sqltest.php
+++ src/http/sqltest.php
@@ -1,9 +1,9 @@
 <?php
-$conn_string  =  "host=localhost port=5432 user=postgres  password=example" ; 
+$conn_string  =  "host=postgres dbname=postgres port=5432 user=postgres  password=example" ; 
 $dbconn = pg_connect($conn_string);
-if (!$dbconn) 
-    echo "postgersql connection success/r/n";
+$stat = pg_connection_status($dbconn);
+if ($stat === PGSQL_CONNECTION_OK) 
+    echo "postgersql connection success\r\n";
 else 
-    echo "failed/r/n";
+    echo "failed\r\n";
     pg_close($dbconn);
-?>
